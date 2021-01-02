afa917fecdb11c83517576e94e8bd9b6de9bc07b
yhlee0 <72429567+yhlee0@users.noreply.github.com>
2021-01-02 21:37:50 +0900

    Add files via upload

---
+++ JumpToPyhton5_4.py
@@ -0,0 +1,22 @@
+'''
+**Info**
+	GitHub name : yhlee0 (fresh2816@naver.com)
+	Subject : JumpToPyhton5_3.py : class
+	URL : https://blog.naver.com/fresh2816/222193132690
+	
+'''
+
+
+class HousePark():
+  lastname = "박"     # 클래스변수
+  def __init__(self, name):
+    self.fullname = self.lastname + name
+  def travel(self, where):
+    print("%s, %s여행을 가다." % (self.fullname, where))
+
+pey = HousePark("응용")  # Create instance, pey
+pey.travel("부산")
+print(pey.fullname)
+
+pes = HousePark("도민")
+pes.travel("통영")
